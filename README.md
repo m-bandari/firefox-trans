7a7e42f26ded8027a89b69631888d7c7c50b086d
Mahesh Bandari <11704574+m-bandari@users.noreply.github.com>
2020-02-14 16:47:40 +0530

    Added Support for JET UI
    - Removed support for keyboard shorcuts
    - Fixed "Unchecked runtime.lastError: Could not establish connection. Receiving end does not exist"

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # trans
 Firefox plugin for Oracle CPQ cloud developers
 
-Extension is used to copy transaction ID of a quote/transaction and to get the XML of the transaction. Use Keyboard Shortcuts, Alt + X to open XML and Alt + C to Copy Transaction ID. The shortcuts default to Option + X and Option + C on Mac, respectively.   
+Extension is used to copy transaction ID of a quote/transaction and to get the XML of the transaction.
 
 Note: To pull XML code of a quote, the respective commerce process should have atleast one Printer Friendly/History XSL View.
--- manifest.js
+++ manifest.js
@@ -1,24 +1,63 @@
+var tranId;
+var procId;
 
-var tranId = getId();
-var procId = getProcId();
-
-browser.runtime.sendMessage({			//sending message to background script
-	value: tranId, value2: procId
+browser.runtime.onConnect.addListener(p => {
+  p.onMessage.addListener(message => {
+    console.log("isLegacy " + message.isLegacy);
+    if (message.isLegacy == "true") {
+      tranId = document.getElementsByName("id")[0].value;
+      procId = document.getElementsByName("bm_cm_process_id")[0].value;
+      p.postMessage({ tid: tranId, pid: procId });
+    } else if (message.isLegacy == "false" && message.tabUrl != "") {
+      var currentTabUrl = message.tabUrl;
+      var currentTabUrlFragmentsArr = currentTabUrl.split("/");
+      tranId = currentTabUrlFragmentsArr[currentTabUrlFragmentsArr.length - 1];
+      var processMetadataUrl =
+        "https://" +
+        currentTabUrlFragmentsArr[2] +
+        "/rest/v8/commerceProcesses/" +
+        currentTabUrlFragmentsArr[5] +
+        "/documents";
+      console.log(processMetadataUrl);
+      fetch(processMetadataUrl)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Fetching process documents failed.");
+          }
+          response.json().then(jsonData => {
+            procId = jsonData.items[0].process.id;
+          });
+        })
+        .catch(error => {
+          console.error("Problem fetching Process ID: ", error);
+        });
+      p.postMessage({ tid: tranId, pid: procId });
+    }
+  });
 });
 
-browser.runtime.onMessage.addListener(copyTrans);
+/*browser.runtime.sendMessage({
+  //sending message to background script
+  value: tranId,
+  value2: procId
+});
 
-function getId() {			//To get transaction ID from the quote page
-  	var tranId = document.getElementsByName("id")[0].value;
-  	return tranId;
+function getId() {
+  //To get transaction ID from the quote page
+  var tranId = document.getElementsByName("id")[0];
+  if (typeof tranId === "undefined") {
+    return 0;
+  }
+  return tranId.value;
 }
 
-function getProcId() {			//To get commerce process ID from the quote page
-	var procId = document.getElementsByName("bm_cm_process_id")[0].value;
-	return procId;
-}
+function getProcId() {
+  //To get commerce process ID from the quote page
+  var procId = document.getElementsByName("bm_cm_process_id")[0].value;
+  return procId;
+}*/
 
-function copyTrans(request) {	//function to copy transaction ID to clipboard
+/*function copyTrans(request) {	//function to copy transaction ID to clipboard
 	if(request.message === "copy") {
 		var tran_Id = document.getElementsByName("id")[0].value;
 		var tranSelectBox = document.createElement("INPUT");
@@ -34,4 +73,4 @@ function copyTrans(request) {	//function to copy transaction ID to clipboard
 			copiedValue: tran_Id
 		});
 	}
-}
+}*/
